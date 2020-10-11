4e146e8f49df3789c72e1f846d2783c5673dd373
demven <dmitry_salnikov@protonmail.com>
2020-10-11 19:49:53 -0400

    Redirect to https only in prod

--- src/main.js
+++ src/main.js
@@ -2,8 +2,9 @@ import Vue from 'vue';
 import TextareaAutosize from 'vue-textarea-autosize';
 import App from './App.vue';
 import router from './router';
+import { env } from './env';
 
-if (typeof window !== 'undefined') {
+if (env.NODE_ENV !== 'development' && typeof window !== 'undefined') {
   if (window.location.protocol !== 'https:') {
     window.location.href = window.location.href.replace('http:', 'https:');
   }
