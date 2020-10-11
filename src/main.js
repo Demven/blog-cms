0ba34b64741811e1412c74828c9ca37124ec07d6
demven <dmitry_salnikov@protonmail.com>
2020-10-11 19:48:35 -0400

    Redirect to https

--- src/main.js
+++ src/main.js
@@ -3,6 +3,12 @@ import TextareaAutosize from 'vue-textarea-autosize';
 import App from './App.vue';
 import router from './router';
 
+if (typeof window !== 'undefined') {
+  if (window.location.protocol !== 'https:') {
+    window.location.href = window.location.href.replace('http:', 'https:');
+  }
+}
+
 Vue.use(TextareaAutosize);
 
 new Vue({
